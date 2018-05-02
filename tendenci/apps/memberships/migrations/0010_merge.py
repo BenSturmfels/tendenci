406ad401c7d8b5054ead88ded7e77bc56e0d94bd
Ben Sturmfels <ben@sturm.com.au>
2018-05-02 16:38:38 +1000

    Add membership merge migration following rebase.

---
+++ tendenci/apps/memberships/migrations/0010_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('memberships', '0009_auto_20171023_0105'),
+        ('memberships', '0008_auto_20180315_0857'),
+    ]
+
+    operations = [
+    ]
